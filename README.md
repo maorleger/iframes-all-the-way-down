ce67b26927ff277f35b24796eebea4b9a217d4d5
Maor Leger <mleger@squareup.com>
2022-10-11 16:49:08 -0700

    add ngrok info and make results easier to spot

--- README.md
+++ README.md
@@ -6,9 +6,20 @@ To run:
 
 Ensure port 3000 is unused.
 
-```
+```bash
 npm install
 npm run start
 ```
 
 Navigate to http://localhost:3000
+
+## Usage with ngrok
+
+There's just one thing to note when using ngrok, by default most browsers
+have a Referrer-Policy that omits the referrer when downgrading from HTTPS to HTTP (strict-origin-when-cross-origin, a stricter version of no-referrer-when-downgrade)...
+
+So, you have to add a Referrer-Policy to the response header.
+
+```bash
+ngrok http 3000 --response-header-add "Referrer-Policy: origin"
+```
--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-// copied to individual folders as part of `npm start`
-console.dir({
-  url: window.location.href,
-  referrer: window.document.referrer,
-  // https://developer.mozilla.org/en-US/docs/Web/API/Location/ancestorOrigins
-  // NOT SUPPORTED IN FF! So we will need to consider another approach for FF unfortunately
-  ancestorOrigins: window.location.ancestorOrigins,
+const appId = ''
+const locationId = ''
+
+async function tokenize(paymentMethod) {
+  const tokenResult = await paymentMethod.tokenize();
+  if (tokenResult.status === 'OK') {
+    return tokenResult.token;
+  } else {
+    let errorMessage = `Tokenization failed with status: ${tokenResult.status}`;
+    if (tokenResult.errors) {
+      errorMessage += ` and errors: ${JSON.stringify(
+        tokenResult.errors
+      )}`;
+    }
+
+    throw new Error(errorMessage);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async function() {
+  if (!window.Square) {
+    throw new Error('Square.js failed to load properly');
+  }
+
+  const payments = window.Square.payments(appId, locationId);
+  console.log(`${window.location.href}: paymentOptions: `, payments["paymentOptions"])
+
+  const card = await payments.card();
+  await card.attach('#card-container');
+
+
+  const cardButton = document.getElementById('card-button');
+  cardButton.addEventListener('click', async function(event) {
+    event.preventDefault();
+    const token = await tokenize(card);
+    console.log("token", token)
+  });
 })
